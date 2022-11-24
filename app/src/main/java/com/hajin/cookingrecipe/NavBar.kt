5d2b88e6d9ac5c30be555f3d7543f58da975ce92
ha jin <w2128@e-mirim.hs.kr>
2022-11-24 20:02:46 +0900

    bottom_navigation_bar 완성,NavBar Recipe Fragment 생성

---
+++ app/src/main/java/com/hajin/cookingrecipe/NavBar.kt
@@ -0,0 +1,13 @@
+package com.hajin.cookingrecipe
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NavBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+
+    }
+}
---
+++ app/src/main/java/com/hajin/cookingrecipe/RecipeFragment.kt
@@ -0,0 +1,4 @@
+package com.hajin.cookingrecipe
+
+class RecipeFragment {
+}
